53de5551b46b5b28285932076739b01d067f7736
NcBoku <2846608691@qq.com>
2023-04-08 22:33:16 +0800

    拦截器添加

---
+++ src/main/java/com/dxy/config/InterceptorConfig.java
@@ -0,0 +1,22 @@
+package com.dxy.config;
+
+import com.dxy.interceptor.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //创建用户拦截器对象并指定其拦截的路径和排除的路径
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/user/login");
+    }
+}
+
--- src/main/java/com/dxy/controller/ClazzController.java
+++ src/main/java/com/dxy/controller/ClazzController.java
@@ -2,11 +2,13 @@ package com.dxy.controller;
 
 import com.dxy.service.ClazzService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/clazz")
+@CrossOrigin
 public class ClazzController {
     @Autowired
     private ClazzService ClazzService;
