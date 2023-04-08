4f3e08f03f5f599f69b9f7d0e077c77ad1a91c27
NcBoku <2846608691@qq.com>
2023-04-08 22:37:10 +0800

    JWT工具类添加

---
+++ src/main/java/com/dxy/util/JwtUtil.java
@@ -0,0 +1,94 @@
+package com.dxy.util;
+
+
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+
+
+public class JwtUtil {
+
+    private static final long EXPIRE = 60 * 1000; //过期时间
+
+    public static final SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);//密钥，动态生成的密钥
+
+    public static String generate(Map<String,Object> claims) {
+        Date nowDate = new Date();
+        Date expireDate = new Date(System.currentTimeMillis() + EXPIRE);
+        Map<String, Object> header = new HashMap<>(2);
+        header.put("typ", "jwt");
+
+        return Jwts.builder().setHeader(header)
+                .setClaims(claims)  //自定义claims
+                .setIssuedAt(nowDate)//当前时间
+                .setExpiration(expireDate) //过期时间
+                .signWith(key)//签名算法和key
+                .compact();
+    }
+
+    public static boolean isSigned(String token){
+        return  Jwts.parser()
+                .setSigningKey(key)
+                .isSigned(token);
+    }
+
+    public static boolean verify(String token){
+        try {
+            Jwts.parser()
+                    .setSigningKey(key)
+                    .parseClaimsJws(token);
+            return true;
+        }catch (JwtException  e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public static Claims getClaim(String token) {
+        Claims claims = null;
+        try {
+            claims = Jwts.parser()
+                    .setSigningKey(key)
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return claims;
+    }
+
+    public static Date getExpiration(String token) {
+        return getClaim(token).getExpiration();
+    }
+
+    public static boolean isExpired(String token) {
+        try {
+            final Date expiration = getExpiration(token);
+            return expiration.before(new Date());
+        } catch (ExpiredJwtException expiredJwtException) {
+            return true;
+        }
+    }
+
+    public static String getPayloadByBase64(String token){
+        String payload = null;
+        if (isSigned(token)) {
+            try {
+                byte[] payload_byte = Base64.getDecoder().decode(token.split("\\.")[1]);
+                payload = new String(payload_byte);
+            }catch (Exception e){
+                e.printStackTrace();
+                return null;
+            }
+        }
+        return payload;
+    }
+
+}
+
