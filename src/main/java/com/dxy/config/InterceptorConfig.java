bf4987560f656a884e25b3bd0457cbee700c24c2
NcBoku <2846608691@qq.com>
2023-04-10 22:17:43 +0800

    upload file基本完成

--- src/main/java/com/dxy/config/InterceptorConfig.java
+++ src/main/java/com/dxy/config/InterceptorConfig.java
@@ -2,8 +2,10 @@ package com.dxy.config;
 
 import com.dxy.interceptor.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,13 +14,27 @@ public class InterceptorConfig extends WebMvcConfigurationSupport {
     @Autowired
     private LoginInterceptor loginInterceptor;
 
+
+    @Value("${thk.imagesRealPath}")
+    private String realPath;
+    @Value("${thk.imagesServerPath}")
+    private String serverPath;
+
+    // 添加静态资源处理器
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(new String[]{serverPath + "/**"}).addResourceLocations("file:/" + realPath);
+
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //创建用户拦截器对象并指定其拦截的路径和排除的路径
         registry.addInterceptor(loginInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/user/login")
-                .excludePathPatterns("/static/**");
+                .excludePathPatterns("/static/**")
+                .excludePathPatterns("/upload");
         super.addInterceptors(registry);
     }
 }
---
+++ src/main/java/com/dxy/controller/UploadController.java
@@ -0,0 +1,42 @@
+package com.dxy.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@RestController
+@CrossOrigin
+public class UploadController {
+    //获取yaml中配置的上传路径属性
+    @Value(("${web.upload-path}"))
+    private String uploadPath;
+
+    @PostMapping("/upload")
+    public String upload(@RequestPart("file") MultipartFile file) throws IOException {
+        String fileName = file.getOriginalFilename();  //获取文件原名
+        String visibleUri="/"+fileName;                //拼接访问图片的地址
+        String saveUri=uploadPath+"/"+fileName;        //拼接保存图片的真实地址
+
+        System.out.println("图片原文件名="+fileName+"图片访问地址="+visibleUri+" 图片保存真实地址={}"+saveUri);
+
+        File saveFile = new File(saveUri);
+        //判断是否存在文件夹，不存在就创建
+        if (!saveFile.exists()){
+            saveFile.mkdirs();
+        }
+        try {
+            file.transferTo(saveFile);  //保存文件到真实存储路径下
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return visibleUri;
+    }
+
+}
